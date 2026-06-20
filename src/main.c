83be7713c0cecf95d106611afe736a87dd739cd5
wfogame <87988761+wfogame@users.noreply.github.com>
2026-06-19 23:45:49 -0400

    fixed main.c to actually provide accurate results

--- src/main.c
+++ src/main.c
@@ -4,8 +4,10 @@
 int main(int argc, char* argv[]) {
     unsigned int height;
     unsigned int width;
-    printf("Enter width then height in this format \n width,height");
+    printf("Enter width then height in this format \n width,height\n");
     scanf("%u,%u", &width,&height); 
-    printf("%p",createMap(width,height));
+    struct pixelmap *mappy = createMap(width,height);
+    printf("%p\n",mappy);
+    printf("Width: %u,Height: %u",(*mappy).width,(*mappy).height);
     return 0;
 }
