aac8825181931214e7f48554ed0ee7072fd40c3f
wfogame <87988761+wfogame@users.noreply.github.com>
2026-06-20 09:14:31 -0400

    Integrated SDL2, now a window will be created

--- src/main.c
+++ src/main.c
@@ -2,12 +2,39 @@
 #include <stdio.h>
 #include "map.h"
 int main(int argc, char* argv[]) {
-    unsigned int height;
-    unsigned int width;
-    printf("Enter width then height in this format \n width,height\n");
-    scanf("%u,%u", &width,&height); 
-    struct pixelmap *mappy = createMap(width,height);
-    printf("%p\n",mappy);
-    printf("Width: %u,Height: %u",(*mappy).width,(*mappy).height);
-    return 0;
+  if(SDL_Init(SDL_INIT_VIDEO)){
+    printf("SDL_Init failed to initiliaze properly");
+  }
+  unsigned int height;
+  unsigned int width;
+  printf("Enter width then height in this format \n width,height\n");
+  scanf("%u,%u", &width,&height); 
+  struct pixelmap *mappy = createMap(width,height);
+  printf("%p\n",mappy);
+  printf("Width: %u,Height: %u",(*mappy).width,(*mappy).height);
+  SDL_Window *win = SDL_CreateWindow("My Map",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1920,1080,0);
+  SDL_Event userevent;
+  SDL_Renderer *winrender = SDL_CreateRenderer(win,-1,0);
+  if(winrender == NULL){
+    printf("Sdl renderer failed to initiliaze");
+    return 1;
+  }
+  int quit = 0;
+  while(!quit){
+    while(SDL_PollEvent(&userevent)){
+      if(userevent.type==SDL_QUIT){
+       quit += 1; 
+      }
+    }
+    SDL_SetRenderDrawColor(winrender,0,0,0,255);
+    SDL_RenderClear(winrender);
+    // Draw stuff here
+    
+    //
+    SDL_RenderPresent(winrender);
+    
+  }
+  SDL_DestroyWindow(win);
+  SDL_Quit();
+  return 0;
 }
