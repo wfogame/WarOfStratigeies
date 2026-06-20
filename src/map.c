708d5402c8cee72f103e69cd425fc23753e4305f
wfogame <87988761+wfogame@users.noreply.github.com>
2026-06-19 23:14:31 -0400

    (Fix): map wasn't properly written

--- src/map.c
+++ src/map.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-int *createMap(unsigned int width,unsigned int height){
-  struct pixelmap *worldMapvariable = malloc(sizeof(struct pixelmap)); 
-  worldMapvariable.width = width;
-  worldMapvariable.height = height;
-  struct pixelmap *worldMap = &worldMapvariable;
+#include <stdlib.h>
+struct pixelmap *createMap(unsigned int width,unsigned int height){
+  struct pixelmap *worldMap = malloc(sizeof(struct pixelmap)); 
+  worldMap -> width = width;
+  worldMap -> height = height;
   worldMap -> pixels = malloc(sizeof(int*) * width);
   for (unsigned int i = 0; i < width; i++) {
     (*worldMap).pixels[i] = malloc(sizeof(int)*height);
   }
-  return &worldMapvariable
+  return worldMap;
 }
 
